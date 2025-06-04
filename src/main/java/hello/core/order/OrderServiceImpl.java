b2806f2dc6a630116e62d51a8373398aa7d0502c
DANIEL_MAC <bnbpower9@naver.com>
2025-06-04 21:37:44 +0900

    스프링강의 50강 애노테이션 만들어보기

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,20 +4,23 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor //필수값인 final 변수들을 가지고 생성자를 생성해줌
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository; //전부 인터페이스만 바라보고 구현체 누구할지는 appconfig가 알아서  처리
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
-    public OrderServiceImpl(DiscountPolicy discountPolicy, MemberRepository memberRepository) {
-        this.discountPolicy = discountPolicy;
-        this.memberRepository = memberRepository;
-    }
+    // required 때문에 필요없어짐
+//    @Autowired
+//    public OrderServiceImpl(Di scountPolicy discountPolicy, MemberRepository memberRepository) {
+//        this.discountPolicy = discountPolicy;
+//        this.memberRepository = memberRepository;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
